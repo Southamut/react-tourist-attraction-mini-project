956c899920e6b11be174cefe36cd71b4cc67873e
Beat <beaterftr@gmail.com>
2025-08-21 13:44:25 +0700

    feat: update search functionality and display function using axios and usestate

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,9 +1,36 @@
 import TripSuggestList from "./TripSuggestList";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 function HomePage() {
+
+    //For Searching
+    const [keywords, setKeywords] = useState("");
+
+    const handleChange = (e) => {
+        setKeywords(e.target.value);
+    }
+
+    //For Displaying
+    const [toDisplay, setToDisplay] = useState([]);
+
+    const getData = async () => {
+        try {
+            const response = await axios.get(`http://localhost:4001/trips?keywords=${keywords}`)
+            console.log(response.data.data);
+            setToDisplay(response.data.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, [keywords]);
+
+
+    //Displaying
     return (
         // window box
         <div className="bg-white w-full border border-red-500">
@@ -15,11 +42,15 @@ function HomePage() {
             {/* search box */}
             <div className="flex flex-col items-center justify-center mt-10 border border-red-700">
                 <label htmlFor="search" className="text-sm font-sans font-bold border border-red-500 text-gray-500 w-9/12 text-left">หาที่เที่ยวแล้วไปกัน</label>
-                <input type="text" placeholder="ค้นหา" className="w-9/12 p-2 rounded-md border border-green-500" />
+                <input type="text"
+                    placeholder="ค้นหา"
+                    className="w-9/12 p-2 rounded-md border border-green-500"
+                    onChange={handleChange}
+                />
             </div>
 
             {/* list of trips */}
-            <TripSuggestList />
+            <TripSuggestList toDisplay={toDisplay} />
         </div>
     )
 }
--- client/src/components/TripSuggestList.jsx
+++ client/src/components/TripSuggestList.jsx
@@ -1,52 +1,64 @@
-function TripSuggestList() {
+function TripSuggestList(props) {
     return (
         <div className="flex flex-col items-center justify-center mt-10 border border-blue-700">
+
             {/* trip-suggest-list */}
-            <div className="w-11/12 border border-green-500 flex flex-col sm:flex-row">
-                {/* left-box */}
-                <div className="w-full sm:w-1/3 border border-red-500">
-                    <img src="https://img.wongnai.com/p/1600x0/2019/07/02/3c758646aa6c426ba3c6a81f57b20bd6.jpg"
-                        alt="trip-image"
-                        className="w-full h-full object-cover rounded-xl md:rounded-3xl"
-                    />
-                </div>
-
-                {/* right-box */}
-                <div className="w-full sm:w-2/3 border border-red-500 flex flex-col">
-                    {/* half-top */}
-                    <div className="border border-blue-500 h-1/2">
-                        <h1 className="text-2xl font-sans font-bold text-gray-800">Trip Suggest List</h1>
-                        <p className="text-sm font-sans font-medium text-gray-500">lets go to travel!!!</p>
-                        <a href="https://www.wongnai.com/trips/travel-koh-chang"
-                            className="text-sm font-sans font-light text-blue-400 underline"
-                        >อ่านต่อ
-                        </a>
+            {props.toDisplay.map((item) => (
+                <div key={item.eid} className="w-11/12 h-60 mb-8 border border-green-500 flex flex-col sm:flex-row">
+                    {/* left-box */}
+                    <div className="w-full sm:w-1/3 border border-red-500">
+                        <img src={item.photos[0]}
+                            alt="trip-image"
+                            className="w-full h-full object-cover rounded-xl md:rounded-3xl"
+                        />
                     </div>
 
-                    {/* half-bottom */}
-                    <div className="border border-blue-500 h-1/2 flex flex-row gap-5 p-3">
-                        <div className="w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 border border-blue-500">
-                            <img
-                                src="https://img.wongnai.com/p/1600x0/2019/07/02/6a2733ab91164ac8943b77deb14fdbde.jpg"
-                                className="w-full h-full object-cover rounded-lg md:rounded-xl"
-                            />
+                    {/* right-box */}
+                    <div className="w-full pl-4 sm:w-2/3 border border-red-500 flex flex-col gap-1">
+                        {/* half-top */}
+                        <div className="border border-green-500 h-1/2">
+                            <a
+                                href={item.url}
+                                className="text-2xl font-sans font-bold text-gray-800 hover:text-orange-400">{item.title}</a>
+                            <p className="text-sm font-sans font-medium text-gray-500">
+                                {item.description.length > 100 ? item.description.slice(0, 100) + "..." : item.description}
+                            </p>
+                            {/* <a href=""
+                                className="text-sm font-sans font-light text-blue-400 underline"
+                            >อ่านต่อ
+                            </a> */}
+                            <div className="flex flex-row gap-2 border border-blue-500">
+                                <div className="border border-blue-500 flex justify-center items-center">
+                                    <p className="text-sm font-medium text-gray-500">หมวด</p>
+                                </div>
+                                {item.tags.map((tag, index) => (
+                                    <div key={index} className="text-sm font-light text-blue-400 underline">
+                                        #{tag}
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                        <div className="w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 border border-blue-500">
-                            <img
-                                src="https://img.wongnai.com/p/1600x0/2019/07/02/6a2733ab91164ac8943b77deb14fdbde.jpg"
-                                className="w-full h-full object-cover rounded-lg md:rounded-xl"
-                            />
-                        </div>
-                        <div className="w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 border border-blue-500">
-                            <img
-                                src="https://img.wongnai.com/p/1600x0/2019/07/02/6a2733ab91164ac8943b77deb14fdbde.jpg"
-                                className="w-full h-full object-cover rounded-lg md:rounded-xl"
-                            />
+
+                        {/* half-bottom */}
+                        <div className="border border-green-200 h-1/2 flex flex-row gap-5 p-3">
+                            <DetailPhoto photo={item.photos[1]} />
+                            <DetailPhoto photo={item.photos[2]} />
+                            <DetailPhoto photo={item.photos[3]} />
                         </div>
                     </div>
                 </div>
-            </div>
+            ))}
+        </div>
+    )
+}
 
+function DetailPhoto(props) {
+    return (
+        <div className="w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 border border-blue-500">
+            <img
+                src={props.photo}
+                className="w-full h-full object-cover rounded-lg md:rounded-xl"
+            />
         </div>
     )
 }
